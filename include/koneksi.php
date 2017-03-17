e5982f638cded275a418d422753c474b68261e47
yudiprastyo <yudi_kren@yahoo.co.id>
2017-03-17 16:12:22 +0700

    Update koneksi.php
    db konek azure update

--- include/koneksi.php
+++ include/koneksi.php
@@ -3,9 +3,9 @@
  *      koneksi.php
  *      Melakukan koneksi ke database
  */
-$user = "root";
+$user = "azure";
 $host = "localhost";
-$pass = "2hm2dafandi";
+$pass = "6#vWHD_$";
 $db = "simdes";
 $conn = mysqli_connect($host,$user,$pass,$db) or die("koneksi gagal");
 
